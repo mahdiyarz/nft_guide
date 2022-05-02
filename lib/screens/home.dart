34d69fe367d8fdf354e9839d8907601e930ab1dc
mahDyarZ <m.arbabzi@gmail.com>
2022-05-02 22:39:01 +0430

    First look for home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,6 +5,61 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: ListView.builder(
+          itemCount: 12,
+          itemBuilder: (context, index) {
+            return InkWell(
+              child: Card(
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 10,
+                  vertical: 5,
+                ),
+                // color: Colors.amberAccent,
+                elevation: 7,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+
+                child: Container(
+                    decoration: BoxDecoration(
+                      gradient: const LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                          Colors.green,
+                          Colors.lightGreen,
+                        ],
+                      ),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    width: MediaQuery.of(context).size.width,
+                    height: 90,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'شماره فصل',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                        FittedBox(
+                          fit: BoxFit.fill,
+                          child: Text(
+                            'موضوع فصل مورد نظر',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                      ],
+                    )),
+              ),
+            );
+          }),
+    );
   }
 }
