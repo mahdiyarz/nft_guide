014cf49c00394c8417d20f2d96424de706203518
mahDyarZ <m.arbabzi@gmail.com>
2022-05-03 14:37:32 +0430

    Config header of details screen

--- lib/screens/chapterDetail.dart
+++ lib/screens/chapterDetail.dart
@@ -1,15 +1,84 @@
 import 'package:flutter/material.dart';
 
 class ChapterDetail extends StatelessWidget {
-  const ChapterDetail({Key? key}) : super(key: key);
+  String title, image, descriptions;
+  ChapterDetail({
+    Key? key,
+    required this.title,
+    required this.descriptions,
+    required this.image,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('موضوع فصل'),
+      backgroundColor: Colors.indigo[400],
+      body: Column(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 150,
+            padding: const EdgeInsets.only(bottom: 15),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(image),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Container(
+              margin: const EdgeInsets.only(
+                top: 95,
+                left: 8,
+                right: 8,
+              ),
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: Colors.indigo[400],
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Colors.white,
+                    child: IconButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      icon: const Icon(Icons.arrow_back_sharp),
+                      color: Colors.indigo[400],
+                    ),
+                  ),
+                  Flexible(
+                    fit: FlexFit.tight,
+                    child: FittedBox(
+                      fit: BoxFit.fill,
+                      alignment: Alignment.center,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Text(
+                          title,
+                          textDirection: TextDirection.rtl,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 25,
+                            color: Colors.white,
+                            shadows: [
+                              Shadow(
+                                blurRadius: 10.0,
+                                color: Colors.white38,
+                                offset: Offset(0.5, 0.5),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
-      body: Container(),
     );
   }
 }
