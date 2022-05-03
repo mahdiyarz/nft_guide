b9f4f8cab7d1c56e38945b4437606cfffe85d84f
mahDyarZ <m.arbabzi@gmail.com>
2022-05-03 21:57:05 +0430

    Create modal bottom sheet for intrduce our self

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,78 +10,218 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.indigo[600],
       body: ListView.builder(
-          itemCount: 12,
-          itemBuilder: (context, index) {
-            return InkWell(
-              onTap: () => Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (_) => ChapterDetail(
-                    title: nftsData[index].title,
-                    descriptions: nftsData[index].descriptions,
-                    image: nftsData[index].image,
-                  ),
+        itemCount: 12,
+        itemBuilder: (context, index) {
+          return InkWell(
+            onTap: () => Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) => ChapterDetail(
+                  title: nftsData[index].title,
+                  descriptions: nftsData[index].descriptions,
+                  image: nftsData[index].image,
                 ),
               ),
-              child: Card(
-                margin: const EdgeInsets.symmetric(
-                  horizontal: 10,
-                  vertical: 5,
-                ),
-                // color: Colors.amberAccent,
-                elevation: 7,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ),
+            ),
+            child: Card(
+              margin: const EdgeInsets.symmetric(
+                horizontal: 10,
+                vertical: 5,
+              ),
+              // color: Colors.amberAccent,
+              elevation: 7,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
 
-                child: Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage(nftsData[index].image),
-                        fit: BoxFit.cover,
-                        opacity: 200,
-                      ),
-                      color: Colors.indigo[300],
-                      // gradient: const LinearGradient(
-                      //   begin: Alignment.topLeft,
-                      //   end: Alignment.bottomRight,
-                      //   colors: [
-                      //     Colors.green,
-                      //     Colors.lightGreen,
-                      //   ],
-                      // ),
-                      borderRadius: BorderRadius.circular(15),
+              child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(nftsData[index].image),
+                      fit: BoxFit.cover,
+                      opacity: 200,
                     ),
-                    width: MediaQuery.of(context).size.width,
-                    height: 120,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          nftsData[index].chapter,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          ),
+                    color: Colors.indigo[300],
+                    // gradient: const LinearGradient(
+                    //   begin: Alignment.topLeft,
+                    //   end: Alignment.bottomRight,
+                    //   colors: [
+                    //     Colors.green,
+                    //     Colors.lightGreen,
+                    //   ],
+                    // ),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  height: 120,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        nftsData[index].chapter,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
                         ),
-                        FittedBox(
-                          fit: BoxFit.fill,
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 15),
-                            child: Text(
-                              nftsData[index].title,
-                              textDirection: TextDirection.rtl,
-                              style: const TextStyle(
-                                fontWeight: FontWeight.w400,
-                                fontSize: 25,
-                              ),
+                      ),
+                      FittedBox(
+                        fit: BoxFit.fill,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 15),
+                          child: Text(
+                            nftsData[index].title,
+                            textDirection: TextDirection.rtl,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 25,
                             ),
                           ),
                         ),
-                      ],
-                    )),
+                      ),
+                    ],
+                  )),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        child: FloatingActionButton(
+          onPressed: () => showModalBottomSheet(
+            context: context,
+            builder: (ctx) => _buildBottomSheet(ctx),
+          ),
+          child: const Icon(Icons.alternate_email_rounded),
+          elevation: 8,
+          backgroundColor: Colors.indigo[300],
+          mini: true,
+        ),
+      ),
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.miniStartDocked,
+    );
+  }
+
+  Container _buildBottomSheet(BuildContext context) {
+    return Container(
+      color: Colors.indigo[300],
+      height: 150,
+      padding: const EdgeInsets.all(8),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              color: Colors.indigo[500],
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: const Text(
+              'طراحی و توسعه',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 22,
+                color: Colors.white,
               ),
-            );
-          }),
+            ),
+          ),
+          GridView(
+            shrinkWrap: true,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 5,
+              mainAxisSpacing: 5,
+              mainAxisExtent: 80,
+            ),
+            children: [
+              Container(
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.indigo[500],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: const [
+                    Flexible(
+                      fit: FlexFit.loose,
+                      child: FittedBox(
+                        fit: BoxFit.cover,
+                        child: Text(
+                          'مهدیار ارباب زی',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Flexible(
+                      fit: FlexFit.loose,
+                      child: FittedBox(
+                        fit: BoxFit.cover,
+                        child: Text(
+                          'm.arbabzi@gmail.com',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.indigo[500],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: const [
+                    Flexible(
+                      fit: FlexFit.loose,
+                      child: FittedBox(
+                        fit: BoxFit.cover,
+                        child: Text(
+                          'سینا زره پوش',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Flexible(
+                      fit: FlexFit.loose,
+                      child: FittedBox(
+                        fit: BoxFit.cover,
+                        child: Text(
+                          'sina.zrp@gmail.com',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
