cb35f19e1cdabe80ad8eeadad013d99860d6df56
mahDyarZ <m.arbabzi@gmail.com>
2022-05-03 20:06:33 +0430

    Set text widget for descriptions on detail screen

--- lib/screens/chapterDetail.dart
+++ lib/screens/chapterDetail.dart
@@ -13,71 +13,89 @@ class ChapterDetail extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.indigo[400],
-      body: Column(
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: 150,
-            padding: const EdgeInsets.only(bottom: 15),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(image),
-                fit: BoxFit.cover,
-              ),
-            ),
-            child: Container(
-              margin: const EdgeInsets.only(
-                top: 95,
-                left: 8,
-                right: 8,
-              ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
               width: MediaQuery.of(context).size.width,
+              height: 150,
+              padding: const EdgeInsets.only(bottom: 15),
               decoration: BoxDecoration(
-                color: Colors.indigo[400],
-                borderRadius: BorderRadius.circular(20),
+                image: DecorationImage(
+                  image: AssetImage(image),
+                  fit: BoxFit.cover,
+                ),
               ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  CircleAvatar(
-                    backgroundColor: Colors.white,
-                    child: IconButton(
-                      onPressed: () => Navigator.of(context).pop(),
-                      icon: const Icon(Icons.arrow_back_sharp),
-                      color: Colors.indigo[400],
+              child: Container(
+                margin: const EdgeInsets.only(
+                  top: 95,
+                  left: 8,
+                  right: 8,
+                ),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: Colors.indigo[400],
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.white,
+                      child: IconButton(
+                        onPressed: () => Navigator.of(context).pop(),
+                        icon: const Icon(Icons.arrow_back_sharp),
+                        color: Colors.indigo[400],
+                      ),
                     ),
-                  ),
-                  Flexible(
-                    fit: FlexFit.loose,
-                    child: FittedBox(
-                      fit: BoxFit.cover,
-                      alignment: Alignment.center,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        child: Text(
-                          title,
-                          textDirection: TextDirection.rtl,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.w400,
-                            fontSize: 20,
-                            color: Colors.white,
-                            shadows: [
-                              Shadow(
-                                blurRadius: 10.0,
-                                color: Colors.white38,
-                                offset: Offset(0.5, 0.5),
-                              ),
-                            ],
+                    Flexible(
+                      fit: FlexFit.loose,
+                      child: FittedBox(
+                        fit: BoxFit.cover,
+                        alignment: Alignment.center,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Text(
+                            title,
+                            textDirection: TextDirection.rtl,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 20,
+                              color: Colors.white,
+                              shadows: [
+                                Shadow(
+                                  blurRadius: 10.0,
+                                  color: Colors.white38,
+                                  offset: Offset(0.5, 0.5),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 10,
+              ),
+              child: Text(
+                descriptions,
+                textDirection: TextDirection.rtl,
+                textAlign: TextAlign.justify,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
