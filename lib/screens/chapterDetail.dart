e69674f75bacc3face4419a86015545440040516
Sina <Sina.zrp@gmail.com>
2022-05-15 01:34:10 +0430

    fix admob and add multy color style

--- lib/screens/chapterDetail.dart
+++ lib/screens/chapterDetail.dart
@@ -3,133 +3,204 @@ import 'package:tapsell_plus/tapsell_plus.dart';
 
 class ChapterDetail extends StatefulWidget {
   String title, image, descriptions;
-  ChapterDetail({
-    Key? key,
-    required this.title,
-    required this.descriptions,
-    required this.image,
-  }) : super(key: key);
+
+  NativeAdData? ad2;
+  ChapterDetail(
+      {Key? key,
+      required this.title,
+      required this.descriptions,
+      required this.image,
+      this.ad2})
+      : super(key: key);
 
   @override
   State<ChapterDetail> createState() => _ChapterDetailState();
 }
 
 class _ChapterDetailState extends State<ChapterDetail> {
-  String responseid = '';
-  void setResponseId(String id) {
-    responseid = id;
-  }
-
   @override
   void initState() {
     // TODO: implement initState
-    TapsellPlus.instance
-        .requestStandardBannerAd(
-            '627e791825def655dd1e4e17', TapsellPlusBannerType.BANNER_320x50)
-        .then((responseId) {
-      setResponseId(responseId);
-      TapsellPlus.instance.showStandardBannerAd(responseId,
-          TapsellPlusHorizontalGravity.BOTTOM, TapsellPlusVerticalGravity.RIGHT,
-          margin: EdgeInsets.only(top: 100), onOpened: (map) {
-        // Ad opened
-      }, onError: (map) {
-        // Error when showing ad
-      });
-      // SAVE the responseId
-    }).catchError((error) {
-      // Error when requesting for an ad
-    });
+
     super.initState();
   }
 
+  Color backgroundColor = Color(0xff707070);
+  Color textColor = Colors.white70;
+
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () {
-        return TapsellPlus.instance.destroyStandardBanner(responseid);
-      },
-      child: Scaffold(
-        backgroundColor: Color(0xff707070),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: 200,
-                padding: const EdgeInsets.only(bottom: 15),
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(widget.image),
-                    fit: BoxFit.cover,
-                  ),
+    return Scaffold(
+      bottomNavigationBar: (widget.ad2 != null)
+          ? BottomAppBar(
+              child: Container(
+              color: Color.fromARGB(255, 0, 0, 0),
+              height: 50,
+              child: Row(children: [
+                ElevatedButton(
+                    onPressed: (() {
+                      TapsellPlus.instance.nativeBannerAdClicked(
+                          widget.ad2!.responseId.toString());
+                    }),
+                    child: Text(
+                      'کلیک کنید',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                    )),
+                Text(
+                  widget.ad2!.description.toString(),
+                  overflow: TextOverflow.fade,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  widget.title,
-                  textDirection: TextDirection.rtl,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 22,
-                    color: Colors.white,
-                    shadows: [
-                      Shadow(
-                        blurRadius: 25.0,
-                        color: Colors.white70,
-                        offset: Offset(0.5, 0.5),
-                      ),
-                    ],
-                  ),
+                IconButton(
+                  icon: Image.network(widget.ad2!.iconUrl.toString()),
+                  onPressed: () {
+                    TapsellPlus.instance.nativeBannerAdClicked(
+                        widget.ad2!.responseId.toString());
+                  },
                 ),
-              ),
-              const Divider(
-                thickness: 2,
-                indent: 45,
-                endIndent: 45,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 15,
-                  vertical: 10,
-                ),
-                child: Text(
-                  widget.descriptions,
-                  textDirection: TextDirection.rtl,
-                  textAlign: TextAlign.justify,
-                  style: const TextStyle(
-                    color: Colors.white70,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 50,
-              )
-            ],
-          ),
-        ),
-        floatingActionButton: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8),
-          child: FloatingActionButton(
-            onPressed: () {
-              TapsellPlus.instance.destroyStandardBanner(responseid);
-              Navigator.of(context).pop();
-            },
-            child: const Icon(
-              Icons.home,
-              color: Colors.white70,
+              ], mainAxisAlignment: MainAxisAlignment.spaceEvenly),
+            ))
+          : BottomAppBar(
+              color: Colors.transparent,
             ),
-            elevation: 8,
-            backgroundColor: const Color.fromARGB(180, 218, 75, 243),
-            mini: true,
-          ),
+      backgroundColor: backgroundColor,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 200,
+              padding: const EdgeInsets.only(bottom: 15),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(widget.image),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                widget.title,
+                textDirection: TextDirection.rtl,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                  color: textColor,
+                  shadows: [
+                    Shadow(
+                      blurRadius: 25.0,
+                      color: Colors.white70,
+                      offset: Offset(0.5, 0.5),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: const Color.fromARGB(180, 218, 75, 243)),
+              width: 120,
+              height: 40,
+              child:
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: InkWell(
+                    onTap: () {
+                      setState(() {
+                        backgroundColor = Color.fromARGB(255, 230, 230, 230);
+                        textColor = Colors.black;
+                      });
+                    },
+                    child: CircleAvatar(
+                      backgroundColor: Color.fromARGB(255, 230, 230, 230),
+                      maxRadius: 15,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: InkWell(
+                    onTap: () {
+                      setState(() {
+                        backgroundColor = Colors.black;
+                        textColor = Colors.white;
+                      });
+                    },
+                    child: CircleAvatar(
+                      backgroundColor: Colors.black,
+                      maxRadius: 15,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: InkWell(
+                    onTap: () {
+                      setState(() {
+                        backgroundColor = Color(0xff707070);
+                        textColor = Colors.white70;
+                      });
+                    },
+                    child: CircleAvatar(
+                      backgroundColor: Color(0xff707070),
+                      maxRadius: 15,
+                    ),
+                  ),
+                ),
+              ]),
+            ),
+            const Divider(
+              thickness: 2,
+              indent: 45,
+              endIndent: 45,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 10,
+              ),
+              child: Text(
+                widget.descriptions,
+                textDirection: TextDirection.rtl,
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                  color: textColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+          ],
         ),
-        floatingActionButtonLocation:
-            FloatingActionButtonLocation.miniStartDocked,
       ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        child: FloatingActionButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Icon(
+            Icons.home,
+            color: Colors.white70,
+          ),
+          elevation: 8,
+          backgroundColor: const Color.fromARGB(180, 218, 75, 243),
+          mini: true,
+        ),
+      ),
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.miniStartDocked,
     );
   }
 }
