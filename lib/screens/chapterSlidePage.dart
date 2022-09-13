f7f0cf088638066686c396e853d900ae657deb0a
mahDyarZ <m.arbabzi@gmail.com>
2022-09-13 22:15:50 +0430

    Create list of markets benefits

--- lib/screens/chapterSlidePage.dart
+++ lib/screens/chapterSlidePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nft_guide/widgets/custom_table.dart';
 
 class ChapterSlidePage extends StatelessWidget {
   // its a slide screen in article page screen
@@ -65,7 +66,7 @@ class ChapterSlidePage extends StatelessWidget {
                       textDirection: TextDirection.rtl,
                       child: Padding(
                         padding: const EdgeInsets.symmetric(
-                            horizontal: 15, vertical: 3),
+                            horizontal: 15, vertical: 8),
                         child: Center(
                             child: FittedBox(
                           fit: BoxFit.cover,
@@ -109,7 +110,7 @@ class ChapterSlidePage extends StatelessWidget {
               starnote != null
                   ? Padding(
                       padding: const EdgeInsets.only(
-                        top: 10,
+                        top: 20,
                         right: 15,
                         left: 15,
                       ),
@@ -150,10 +151,86 @@ class ChapterSlidePage extends StatelessWidget {
                       ),
                     )
                   : SizedBox(),
+              // benefits != null
+              //     ?
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 20,
+                  right: 15,
+                  left: 15,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      alignment: Alignment.bottomRight,
+                      padding: EdgeInsets.only(right: _width / 35),
+                      child: Text('نکات مثبت',
+                          textAlign: TextAlign.right,
+                          style: TextStyle(color: Colors.white)),
+                    ),
+                    Container(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            ListView.builder(
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemCount: 5,
+                              itemBuilder: (context, index) => Directionality(
+                                textDirection: TextDirection.rtl,
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Icon(
+                                      Icons.check_circle,
+                                      color: Color.fromARGB(255, 142, 183, 65),
+                                    ),
+                                    SizedBox(width: 8),
+                                    Flexible(
+                                      child: Text(describtion,
+                                          style: TextStyle(
+                                            color: Color.fromARGB(
+                                                255, 171, 171, 171),
+                                            fontSize: 18.5,
+                                            fontWeight: FontWeight.normal,
+                                            height: 1.55,
+                                            fontFamily: 'Vazir',
+                                          ),
+                                          textDirection: TextDirection.rtl,
+                                          textAlign: TextAlign.justify),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 52, 56, 59),
+                          borderRadius: BorderRadius.circular(8)),
+                    ),
+                  ],
+                ),
+              ),
+              // : const SizedBox(),
+              Directionality(
+                textDirection: TextDirection.rtl,
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    top: 20,
+                    right: 15,
+                    left: 15,
+                  ),
+                  child: Container(),
+                ),
+              ),
               image2 != null
                   ? Padding(
                       padding: const EdgeInsets.only(
-                        top: 10,
+                        top: 20,
                         right: 15,
                         left: 15,
                       ),
@@ -171,7 +248,7 @@ class ChapterSlidePage extends StatelessWidget {
               describtion2 != null
                   ? Padding(
                       padding: const EdgeInsets.only(
-                        top: 10,
+                        top: 20,
                         right: 15,
                         left: 15,
                       ),
@@ -198,7 +275,7 @@ class ChapterSlidePage extends StatelessWidget {
               starnote2 != null
                   ? Padding(
                       padding: const EdgeInsets.only(
-                        top: 10,
+                        top: 20,
                         right: 15,
                         left: 15,
                       ),
@@ -238,36 +315,9 @@ class ChapterSlidePage extends StatelessWidget {
                       ),
                     )
                   : SizedBox(),
-              // benefits != null
-              //     ?
-              Padding(
-                padding: const EdgeInsets.only(
-                  top: 10,
-                  right: 15,
-                  left: 15,
-                ),
-                child: Container(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(describtion,
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 171, 171, 171),
-                          fontSize: 18.5,
-                          fontWeight: FontWeight.normal,
-                          height: 1.55,
-                          fontFamily: 'Vazir',
-                        ),
-                        textDirection: TextDirection.rtl,
-                        textAlign: TextAlign.center),
-                  ),
-                  decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 52, 56, 59),
-                      borderRadius: BorderRadius.circular(8)),
-                ),
-              ),
-              // : const SizedBox(),
+
               SizedBox(
-                height: 10,
+                height: 20,
               )
             ],
           ),
