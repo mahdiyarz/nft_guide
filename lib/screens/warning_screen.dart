da5a6b768da79f9997c2b924c0c572d520ac37cc
Sina Zerehpoosh <sina.zrp@gmail.com>
2022-10-07 18:50:25 +0330

    Fix warning Page

--- lib/screens/warning_screen.dart
+++ lib/screens/warning_screen.dart
@@ -16,45 +16,47 @@ class WarningScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 39, 39, 39),
       body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset('assets/images/warning.png'),
-          Text(
-            'توجه ',
-            style: TextStyle(
-                fontSize: 22, fontWeight: FontWeight.bold, color: Colors.red),
-          ),
-          Container(
-            margin: EdgeInsets.all(16),
-            child: Text(
-                'بازار NFT بازار پر ریسکی است و این احتمال وجود دارد که شما تمام سرمایه خود را از دست بدهید ، این برنامه فقط اطلاعاتی جهت آشنایی بیشتر شما با NFT در اختیار شما قرار می دهد و هیچ گونه مسولیتی در قبال ضرر و زیان شما در این بازار ندارد. برای ورود به این بازار پر خطر شما نیاز به دانش بسیار بالایی دارید و نمی توانید فقط با تکیه به اطلاعات این برنامه وارد بازار شوید.',
-                style: TextStyle(
-                  color: Color.fromARGB(255, 171, 171, 171),
-                  fontSize: 18.5,
-                  fontWeight: FontWeight.normal,
-                  height: 1.55,
-                  fontFamily: 'Vazir',
-                ),
-                textDirection: TextDirection.rtl,
-                textAlign: TextAlign.justify),
-          ),
-          Center(
-            child: ElevatedButton(
-                style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(
-                  Color.fromARGB(255, 142, 183, 65),
-                )),
-                onPressed: () {
-                  addBoolToSF();
-                  Navigator.pushAndRemoveUntil(
-                      context,
-                      MaterialPageRoute(builder: (context) => Home()),
-                      (route) => false);
-                },
-                child: Text('قبول می کنم')),
-          )
-        ],
+          child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/warning.png'),
+            Text(
+              'توجه ',
+              style: TextStyle(
+                  fontSize: 22, fontWeight: FontWeight.bold, color: Colors.red),
+            ),
+            Container(
+              margin: EdgeInsets.all(16),
+              child: Text(
+                  'بازار NFT بازار پر ریسکی است و این احتمال وجود دارد که شما تمام سرمایه خود را از دست بدهید ، این برنامه فقط اطلاعاتی جهت آشنایی بیشتر شما با NFT در اختیار شما قرار می دهد و هیچ گونه مسولیتی در قبال ضرر و زیان شما در این بازار ندارد. برای ورود به این بازار پر خطر شما نیاز به دانش بسیار بالایی دارید و نمی توانید فقط با تکیه به اطلاعات این برنامه وارد بازار شوید.',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 171, 171, 171),
+                    fontSize: 18.5,
+                    fontWeight: FontWeight.normal,
+                    height: 1.55,
+                    fontFamily: 'Vazir',
+                  ),
+                  textDirection: TextDirection.rtl,
+                  textAlign: TextAlign.justify),
+            ),
+            Center(
+              child: ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                    Color.fromARGB(255, 142, 183, 65),
+                  )),
+                  onPressed: () {
+                    addBoolToSF();
+                    Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(builder: (context) => Home()),
+                        (route) => false);
+                  },
+                  child: Text('قبول می کنم')),
+            )
+          ],
+        ),
       )),
     );
   }
