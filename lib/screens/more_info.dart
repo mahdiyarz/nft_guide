c072bb400533be1878e82d51865cd300b95d3739
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2022-11-07 02:49:17 +0330

    Update more_info.dart

--- lib/screens/more_info.dart
+++ lib/screens/more_info.dart
@@ -1,6 +1,8 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+import '../widgets/back_button.dart';
+import '../widgets/logo.dart';
+import '../widgets/sign.dart';
+import '../widgets/texts_on_menu.dart';
 
 class MoreInformation extends StatelessWidget {
   const MoreInformation({Key? key}) : super(key: key);
@@ -29,114 +31,32 @@ class MoreInformation extends StatelessWidget {
                 ),
                 child: Stack(
                   children: [
-                    Positioned(
-                      left: _width / 90,
-                      top: _width / 90,
-                      child: Container(
-                        width: 35,
-                        height: 35,
-                        decoration: BoxDecoration(
-                          color: Colors.white38,
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: IconButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          icon: Icon(Icons.arrow_forward_rounded),
-                          color: Color.fromARGB(255, 115, 102, 68),
-                          iconSize: 20,
-                          alignment: Alignment.center,
-                          padding: EdgeInsets.all(5),
-                        ),
-                      ),
-                    ),
+                    MyBackButton(width: _width),
                     Column(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        CircleAvatar(
-                          backgroundColor: Colors.white70,
-                          radius: 55,
-                          child: Image.asset(
-                            'assets/logo/pars_string.png',
-                            fit: BoxFit.cover,
-                          ),
-                        ),
+                        ParsStringLogo(),
                         Column(
                           children: [
-                            Padding(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 20,
-                                vertical: 5,
-                              ),
-                              child: Text(
-                                'این نرم افزار جهت آگاهی بیشتر از تکنولوژی نو ظهور NFT برای شما کاربر گرامی توسط گروه نرم افزاری پارس استرینگ تهیه و تولید شده است.',
-                                style: TextStyle(
-                                  color: Colors.white70,
-                                  fontSize: 16,
-                                ),
-                                textDirection: TextDirection.rtl,
-                                textAlign: TextAlign.justify,
-                                textScaleFactor:
-                                    ScaleSize.textScaleFactor(context),
-                              ),
+                            TextsOnMenu(
+                              text:
+                                  'این نرم افزار جهت آگاهی بیشتر از تکنولوژی نو ظهور NFT برای شما کاربر گرامی توسط گروه نرم افزاری پارس استرینگ تهیه و تولید شده است.',
+                              isBold: false,
                             ),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 20,
-                                vertical: 5,
-                              ),
-                              child: Text(
-                                'لازم به ذکر است که بازارهای مالی مخصوصا ارز های دیجیتال دارای نوسانات زیادی هستند. بنابراین صرفا با اطلاعات ارائه شده توسط این نرم افزار اقدام به سرمایه گذاری در این بازارها را نکنید.',
-                                style: TextStyle(
-                                  color: Colors.white70,
-                                  fontSize: 16,
-                                ),
-                                textDirection: TextDirection.rtl,
-                                textAlign: TextAlign.justify,
-                                textScaleFactor:
-                                    ScaleSize.textScaleFactor(context),
-                              ),
+                            TextsOnMenu(
+                              text:
+                                  'لازم به ذکر است که بازارهای مالی مخصوصا ارز های دیجیتال دارای نوسانات زیادی هستند. بنابراین صرفا با اطلاعات ارائه شده توسط این نرم افزار اقدام به سرمایه گذاری در این بازارها را نکنید.',
+                              isBold: false,
                             ),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 20,
+                            TextsOnMenu(
+                                text:
+                                    'هرگونه کپی برداری از اطلاعات مندرج در این نرم افزار بلامانع است. در صورت تمایل با ذکر منبع از ما حمایت کنید.',
                                 vertical: 10,
-                              ),
-                              child: Text(
-                                'هرگونه کپی برداری از اطلاعات مندرج در این نرم افزار بلامانع است. در صورت تمایل با ذکر منبع از ما حمایت کنید.',
-                                style: TextStyle(
-                                  color: Colors.white70,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                                textDirection: TextDirection.rtl,
-                                textAlign: TextAlign.right,
-                                textScaleFactor:
-                                    ScaleSize.textScaleFactor(context),
-                              ),
-                            ),
+                                isBold: true),
                           ],
                         ),
                         const SizedBox(),
-                        Column(
-                          children: const [
-                            Text(
-                              'Designed & Developed by Pars String Group',
-                              style: TextStyle(
-                                color: Colors.white54,
-                                fontSize: 10,
-                              ),
-                            ),
-                            Text(
-                              'pars.string@gmail.com',
-                              style: TextStyle(
-                                color: Colors.white54,
-                                fontSize: 10,
-                              ),
-                            )
-                          ],
-                        )
+                        ParsStringSign(),
                       ],
                     ),
                   ],
@@ -147,14 +67,5 @@ class MoreInformation extends StatelessWidget {
         ),
       ),
     );
-  }
-}
-
-class ScaleSize {
-  static double textScaleFactor(BuildContext context,
-      {double maxTextScaleFactor = 2}) {
-    final width = MediaQuery.of(context).size.width;
-    double val = (width / 1400) * maxTextScaleFactor;
-    return max(1, min(val, maxTextScaleFactor));
   }
 }
